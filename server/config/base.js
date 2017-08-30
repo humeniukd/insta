3b508b858a3eba9bf1edeb16aca32e502e06bbea
Dmitry Gumenyuk <dmitry.gumenyuk@gmail.com>
2017-08-30 12:52:20 +0200

    Update base.js

--- server/config/base.js
+++ server/config/base.js
@@ -1,8 +1,8 @@
 /* eslint camelcase: 0 */
 const client_secret = ''
 const authUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
-const tokenUrl = 'https://www.googleapis.com/oauth2/v4/token'
-const client_id = '304014048213-ovo201esthvp8mdga35i85dmcjt5ehiv.apps.googleusercontent.com'
+const tokenUrl = 'apps.googleusercontent.com
+const client_id = ''
 const callbackUrl = '/oauth2callback'
 const staticRegexp = /\.([0-9a-z]+)(?:[?#]|$)/i
 const scope = 'email%20profile'
