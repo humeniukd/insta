35bccabd5c31229e35c2d9f4b49dd7ccf2b6c878
Dmitry Gumenyuk <dmitry.gumenyuk@gmail.com>
2017-08-30 12:52:35 +0200

    Update base.js

--- server/config/base.js
+++ server/config/base.js
@@ -1,7 +1,7 @@
 /* eslint camelcase: 0 */
 const client_secret = ''
 const authUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
-const tokenUrl = 'apps.googleusercontent.com
+const tokenUrl = 'apps.googleusercontent.com'
 const client_id = ''
 const callbackUrl = '/oauth2callback'
 const staticRegexp = /\.([0-9a-z]+)(?:[?#]|$)/i
