51c7f0a25760bc46944a81aadbf1b57637b89443
Dmitry Gumenyuk <dmitry.gumenyuk@gmail.com>
2017-08-30 12:53:48 +0200

    Update base.js

--- server/config/base.js
+++ server/config/base.js
@@ -1,18 +1,18 @@
 /* eslint camelcase: 0 */
-const client_secret = 'ntY4juSYvRrwbxmpjGqHf4-H'
+const client_secret = ''
 const authUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
 const tokenUrl = 'https://www.googleapis.com/oauth2/v4/token'
-const client_id = '304014048213-ovo201esthvp8mdga35i85dmcjt5ehiv.apps.googleusercontent.com'
+const client_id = ''
 const callbackUrl = '/oauth2callback'
 const staticRegexp = /\.([0-9a-z]+)(?:[?#]|$)/i
 const scope = 'email%20profile'
 const sessionIdKey = 'x-access-token'
-const email = 'instantcarz@gmail.com'
+const email = 'instantcarz'
 const smtp = {
   service: 'Gmail',
   auth: {
     user: email,
-    pass: 'a24891792a'
+    pass: ''
   }
 };
 
