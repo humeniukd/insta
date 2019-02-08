367b3c700e0812bd7548c90d3ad7232c80dfd92e
foobar <foo@bar>
2019-02-08 16:12:02 +0100

    staticUrl not to be confused with oauth redirect fix

--- gulp/build.js
+++ gulp/build.js
@@ -2,7 +2,7 @@ import gulp from 'gulp'
 import webpack from 'webpack'
 import config from '../webpack.backend'
 
-gulp.task('build', ['clean', 'client', 'docs'], (cb) => {
+gulp.task('build', ['client', 'docs'], (cb) => {
   webpack(config, (err, stats) => {
     stats && console.log(stats.toString())
     err && console.log(err)
--- server/utils/error-handler.js
+++ server/utils/error-handler.js
@@ -1,4 +1,3 @@
 export default (err, req, res, next) => {
-  console.error('asdf', err.stack)
   return res.status(err.statusCode || 500).send(err.msg)
 }
